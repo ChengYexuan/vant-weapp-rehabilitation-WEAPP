fa6fa42808e1986948d5ff3f9ead8cb876c2db2b
BuaichiCai <caijy@zju.edu.cn>
2020-12-31 11:10:32 +0800

    add history, me

--- pages/history/historydetails.js
+++ pages/history/historydetails.js
@@ -1,31 +1,12 @@
-// pages/history/history.js
+// pages/history/historydetails.js
 var util = require('../../utils/utiltime.js');
 Page({
-  data: {
-    show: true,
-    currentDate: new Date().getTime(),
-    maxDate: new Date().getTime(),
-    formatter(type, value) {
-      if (type === 'year') {
-        return `${value}年`;
-      } else if (type === 'month') {
-        return `${value}月`;
-      }
-      return value;
-    },
-  },
-  
-  // 事件处理函数
-  onConfirm() {
-    wx.navigateTo({
-      url: '../history/historydetails?value=value'
-    })
-  },
 
-  onInput(event) {
-    this.setData({
-      currentDate: event.detail,
-    });
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    activeNames: ['1'],
   },
 
   /**
@@ -40,6 +21,18 @@ Page({
     });
   },
 
+  onChange(event) {
+    this.setData({
+      activeNames: event.detail,
+    });
+  },
+  onOpen(event) {
+    Toast(`展开: ${event.detail}`);
+  },
+  onClose(event) {
+    Toast(`关闭: ${event.detail}`);
+  },
+
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
@@ -47,15 +40,11 @@ Page({
 
   },
 
+  /**
+   * 生命周期函数--监听页面显示
+   */
   onShow: function () {
-    // 每个 tab 页下的自定义 tabBar 组件实例是不同的，可通过自定义组件下的 getTabBar 接口，获取当前页面的自定义 tabBar 组件实例。
-    // 不然会发生tabbar中selected与实际上的index不同
-    if (typeof this.getTabBar === 'function' &&
-      this.getTabBar()) {
-      this.getTabBar().setData({
-        selected: 3
-      })
-    }
+
   },
 
   /**
